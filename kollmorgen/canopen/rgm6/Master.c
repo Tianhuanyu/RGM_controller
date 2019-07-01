0a4c9b7d80e2b5750f45d97b09fedefe24fdd4b3
thy <fsfsthy@gmail.com>
2019-07-01 12:49:45 +0800

    重构

--- kollmorgen/canopen/rgm6/Master.c
+++ kollmorgen/canopen/rgm6/Master.c
@@ -19,6 +19,7 @@ You should have received a copy of the GNU Lesser General Public
 License along with this library; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
+// 重构任务
 
 #include "Master.h"
 #include "rgm6.h"
